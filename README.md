45c10dd6ff00c1438dc00a5620315f37bb994281
roflolilolmao <qjeanmon@gmail.com>
2021-08-07 21:27:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tree Sitter Markdown
+
+I'm just fooling around, don't mind this repo
---
+++ grammar.js
@@ -0,0 +1,7 @@
+module.exports = grammar({
+    name: 'markdown',
+
+    rules: {
+        source_file: $ => 'hello'
+    }
+});
